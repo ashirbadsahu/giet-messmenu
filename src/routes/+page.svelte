26e8cbb5cf4aac120162649cf6c2c6e1cd292822
ashirbadsahu <ashirbadsahu@gmail.com>
2023-09-09 16:01:27 +0530

    added function for choose date then data will display

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,40 @@
 <script>
     import { menus } from "$lib/index";
-    
-    let selectedDay = "";
-    let submitted = false;
-    
-    function handleSubmit(event) {
-        event.preventDefault();
-        submitted = true;
-    }
+    let selectedDay = null; // State variable to store the selected day
 
-    function handleStateChange(event) {
-        selectedState = event.target.value;
+    // Function to update the selected day when a button is clicked
+    function selectDay(day) {
+        selectedDay = day;
     }
 </script>
-
-
-    <div>
-        <select name="" id="">
-            {#each menus as menu}
-               <option value={menu}>{menu.day}</option> 
-            {/each}
-        </select>    
+<div class="flex items-center justify-center my-4 text-white flex-col">
+    <h1>GIET Menu for Central Mess</h1>
+    <p class="py-1">Month: September</p>
+</div>
+<div class="flex items-center justify-center my-4">
+    <div class="flex items-center justify-center my-4">
+        {#each menus as menu}
+        <button
+            class="bg-transparent outline-none text-sm border border-white-100 px-3 py-1 text-white uppercase hover:bg-white hover:text-black"
+            on:click={() => selectDay(menu.day)}>
+            {menu.day}
+        </button>
+        {/each}
     </div>
-    
-  
+</div>
 
+<div class="flex items-center justify-center my-10">
+    {#if selectedDay !== null}
+    <div class="w-64 h-68 rounded-lg border-2 border-white text-center">
+        <div class="py-2 px-1">
+        <p class="text-white py-2">Breakfast: {menus.find(menu => menu.day === selectedDay).breakfast}</p>
+        <p class="text-white py-2">Lunch: {menus.find(menu => menu.day === selectedDay).lunch}</p>
+        <p class="text-white py-2">Snacks: {menus.find(menu => menu.day === selectedDay).snacks}</p>
+        <p class="text-white py-2">Dinner: {menus.find(menu => menu.day === selectedDay).dinner}</p>
+        </div>
+
+    </div>
+    {:else}
+    <p class="text-white">Select a day to view the menu.</p>
+    {/if}
+</div>
