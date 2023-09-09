82682a880596bbb433af66e33bcceec81444bc69
ashirbadsahu <ashirbadsahu@gmail.com>
2023-09-09 15:12:25 +0530

    basic setup

---
+++ src/lib/index.js
@@ -0,0 +1,52 @@
+// place files you want to import through the `$lib` alias in this folder.
+export const menus = [
+    {
+        day: "sun",
+        breakfast: "Puri, Upma, Alu-curry",
+        lunch: "Rice, Roti, Dal, Rasam, Fish curry/Paneer and Green Peas Curry, Vegetable Chips",
+        snacks: "Coffee and Biscuits",
+        dinner: "Rice, Roti, Dal, Chhole Masala, Lady Finger/Raw Banana Fry"
+    },
+    {
+        day: "mon",
+        breakfast: "",
+        lunch: "",
+        snacks: "",
+        dinner: ""
+    },
+    {
+        day: "tue",
+        breakfast: "",
+        lunch: "",
+        snacks: "",
+        dinner: ""
+    },
+    {
+        day: "wed",
+        breakfast: "",
+        lunch: "",
+        snacks: "",
+        dinner: ""
+    },
+    {
+        day: "thu",
+        breakfast: "",
+        lunch: "",
+        snacks: "",
+        dinner: ""
+    },
+    {
+        day: "fri",
+        breakfast: "",
+        lunch: "",
+        snacks: "",
+        dinner: ""
+    },
+    {
+        day: "sat",
+        breakfast: "",
+        lunch: "",
+        snacks: "",
+        dinner: ""
+    },
+]
---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { menus } from "$lib/index";
+    
+    let selectedDay = "";
+    let submitted = false;
+    
+    function handleSubmit(event) {
+        event.preventDefault();
+        submitted = true;
+    }
+
+    function handleStateChange(event) {
+        selectedState = event.target.value;
+    }
+</script>
+
+
+    <div>
+        <select name="" id="">
+            {#each menus as menu}
+               <option value={menu}>{menu.day}</option> 
+            {/each}
+        </select>    
+    </div>
+    
+  
+
