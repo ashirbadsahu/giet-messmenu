7168394e0aae8cd61cc46f65ce05761be98c759a
ashirbadsahu <ashirbadsahu@gmail.com>
2023-09-09 16:42:46 +0530

    added datas of menu

--- src/lib/index.js
+++ src/lib/index.js
@@ -9,44 +9,44 @@ export const menus = [
     },
     {
         day: "mon",
-        breakfast: "",
-        lunch: "",
-        snacks: "",
-        dinner: ""
+        breakfast: "Vegetable Upma/Semiya Upma, Alu-Matar-Curry",
+        lunch: "Rice, Roti, Dal, Sambar, Veg curry, Vegetables Chips",
+        snacks: "Tea and Biscuits",
+        dinner: "Rice, Roti, Dal, EggCurry/Mix Veg, Pickle",
     },
     {
         day: "tue",
-        breakfast: "",
-        lunch: "",
-        snacks: "",
-        dinner: ""
+        breakfast: "Bara(4 pc, Upma, Alu-Matar-Curry, Chutney)",
+        lunch: "Rice, Roti, Dal, Egg kasa/Brinjal masala, Fryums chips",
+        snacks: "Tea and Biscuits",
+        dinner: "Roti, Dal, Veg Biriyani, Paneer & Green Matar curry, Sweet Pickle"
     },
     {
         day: "wed",
-        breakfast: "",
-        lunch: "",
-        snacks: "",
-        dinner: ""
+        breakfast: "Vegetable Chow mein",
+        lunch: "Rice, Roti, Dal, Chicken kasa/Paneer alu masala",
+        snacks: "Coffee and Biscuits",
+        dinner: "Rice, Roti, Dalma, Parwal/Kankada, Alu Fry"
     },
     {
         day: "thu",
-        breakfast: "",
-        lunch: "",
-        snacks: "",
-        dinner: ""
+        breakfast: "Poha/Suji halwa, Alu-matar-curry",
+        lunch: "Rice, Roti, Dal, Rasam, Egg masala/Veg masala, Alu choka",
+        snacks: "Tea and Biscuits",
+        dinner: "Roti, Lemon Rice, Dal, Parwal(kankada) Alu curry"
     },
     {
         day: "fri",
-        breakfast: "",
-        lunch: "",
-        snacks: "",
-        dinner: ""
+        breakfast: "Idli, Sambar, Chutney",
+        lunch: "Rice, Roti, Dal, Chicken Masala/Kabuli Paneer, Pampad",
+        snacks: "Tea and Biscuits",
+        dinner: "Roti, Rice, Jhani/Lau chana dal, Alu curry, Kheer"
     },
     {
         day: "sat",
-        breakfast: "",
-        lunch: "",
-        snacks: "",
-        dinner: ""
+        breakfast: "Uthapam/Chakuli,Alu-curry, Chutney",
+        lunch: "Rice, Roti, Dal, Rasam, Egg chilli/ Soyabean chilli, Dahi boondi",
+        snacks: "Tea and Biscuits",
+        dinner: "Roti, Piece Pulao, Dal, Vegetable curry, Aladi/Lvy Gourd Fry"
     },
 ]
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,10 +12,10 @@
     <p class="py-1">Month: September</p>
 </div>
 <div class="flex items-center justify-center my-4">
-    <div class="flex items-center justify-center my-4">
+    <div class="flex items-center justify-center my-4 ">
         {#each menus as menu}
         <button
-            class="bg-transparent outline-none text-sm border border-white-100 px-3 py-1 text-white uppercase hover:bg-white hover:text-black"
+            class="bg-transparent outline-none text-sm border border-white-100 px-2 py-1 text-white uppercase hover:bg-white hover:text-black"
             on:click={() => selectDay(menu.day)}>
             {menu.day}
         </button>
@@ -37,4 +37,17 @@
     {:else}
     <p class="text-white">Select a day to view the menu.</p>
     {/if}
+</div>
+
+<div class="flex items-center justify-center my-10 text-white flex-col">
+    <p class="text-md">Timings</p>
+    <p>Breakfast: 6:30 to 8:30 AM</p>
+    <p>Lunch: 12:15 PM to 2:30 PM</p>
+    <p>Tea: 6:00 PM to 7:00 PM</p> <br>
+    <p class="text-center justify-center text-md">Sunday and holiday</p>
+    <p class="text-center justify-center">Breakfast: 8.00 AM to 9:30 AM</p>
+    <p class="text-center justify-center">and Lunch: 12:30PM to 2:30 PM</p>
+    
+    
+
 </div>
