7c6dc55d6db26a844f99f2d4ea87693d1475d3c7
ashirbadsahu <ashirbadsahu@gmail.com>
2023-09-12 21:41:50 +0530

    made responsive for desktops

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,14 +8,14 @@
     }
 </script>
 <div class="flex items-center justify-center my-4 text-white flex-col">
-    <h1 class="text-xl">GIET Menu for Central Mess</h1>
-    <p class="py-2 text-lg">Month: September</p>
+    <h1 class="text-xl lg:text-2xl">GIET Menu for Central Mess</h1>
+    <p class="py-2 text-lg lg:text:xl">Month: September</p>
 </div>
 <div class="flex items-center justify-center my-4">
     <div class="flex items-center justify-center my-4 ">
         {#each menus as menu}
         <button
-            class="bg-transparent outline-none text-sm border border-white-100 px-2 py-1 text-white uppercase hover:bg-white hover:text-black"
+            class="bg-transparent outline-none text-sm lg:text-lg border border-white-100 px-2 py-1 text-white uppercase hover:bg-white hover:text-black"
             on:click={() => selectDay(menu.day)}>
             {menu.day}
         </button>
@@ -23,9 +23,9 @@
     </div>
 </div>
 
-<div class="flex items-center justify-center my-10">
+<div class="flex items-center justify-center my-10 lg:text-lg">
     {#if selectedDay !== null}
-    <div class="w-64 h-68 rounded-lg border-2 border-white text-center">
+    <div class="w-64 h-68 lg:w-84 lg:h-88 rounded-lg border-2 border-white text-center">
         <div class="py-2 px-1">
         <p class="text-white py-2">Breakfast: {menus.find(menu => menu.day === selectedDay).breakfast}</p>
         <p class="text-white py-2">Lunch: {menus.find(menu => menu.day === selectedDay).lunch}</p>
@@ -39,7 +39,7 @@
     {/if}
 </div>
 
-<div class="flex items-center justify-center my-10 text-white flex-col">
+<div class="flex items-center justify-center my-10 text-white flex-col lg:text-lg">
     <p class="text-md">Timings</p>
     <p>Breakfast: 6:30 to 8:30 AM</p>
     <p>Lunch: 12:15 PM to 2:30 PM</p>
@@ -50,6 +50,6 @@
     <p class="text-center justify-center">and Lunch: 12:30PM to 2:30 PM</p> 
 </div>
 
-<div class="bottom-0 left-0 w-full bg-transparent text-white text-center p-2">
+<div class="bottom-0 left-0 w-full bg-transparent text-white text-center  p-2">
  <a href="https://github.com/ashirbadsahu/" target="_blank">Made with ❤️ by Ashirbad</a> 
 </div>
